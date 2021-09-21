5572149dc10642cdb07e07b7e560ab9b07661319
Akishino1998 <ekopujianto48@gmail.com>
2021-09-21 22:49:00 +0800

    gaji

--- layout/menu.php
+++ layout/menu.php
@@ -4,7 +4,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html" target="_blank" style="display: flex;">
+        <a class="navbar-brand m-0" href="#"  style="display: flex;">
             <img src="assets/img/logo-ct.png" class="navbar-brand-img h-100 mt-1" alt="main_logo">
             <div style="display:inline-block;">
                 <span class="ms-1">Hi, <?php echo $_SESSION['name'] ?></span><br>
